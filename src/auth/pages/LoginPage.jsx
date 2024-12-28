36fdf6c76ffd3d93673c15ffa4503482d149bcf9
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2024-12-28 20:05:09 +0100

    Login page style V1

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,54 @@
+import { Button, Grid, TextField, Typography } from "@mui/material"
 
 export const LoginPage = () => {
   return (
-    <div>
+    <Grid
+      container
+      spacing={ 0 }
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+      <Grid item
+        className="box-shadow"
+        xs={ 3 }
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
 
-        LoginPage
+        <form>
+          <Grid container>
+            <Grid item xs={ 12 } sx={{ mt: 2}}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='correo@google.com' 
+                fullWidth
+              /> 
+            </Grid>
 
-    </div>
+            <Grid item xs={ 12 } sx={{ mt: 2}}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder='Contraseña' 
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={ 2 } sx={{ mb: 2 }}>
+              <Grid item>
+                <Button variant="contained" fullWidth>
+                  Log In
+                </Button>
+              </Grid>
+            </Grid>
+            
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
   )
 }
+
